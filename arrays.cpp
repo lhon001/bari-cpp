8e0fa75134d7d41fc67b833303b8a58a64de59e2
lawrence <lhon001@gmail.com>
2020-12-19 17:17:32 -0800

    array basics

--- arrays.cpp
+++ arrays.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	int arr[10] = {5, 9, 2, 1, 6, 3, 8, 4, 7, 0};
+
+	for (int i = 0; i < 10; i++) {
+		std::cout << arr[i] << std::endl;
+	}
+
 	return 0;
 }
