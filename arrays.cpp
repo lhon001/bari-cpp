7ce0918a5c2c6942716ebc515e6a2c9c1af6f5e3
lawrence <lhon001@gmail.com>
2020-12-19 17:27:10 -0800

    fix formatting

--- arrays.cpp
+++ arrays.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
 
 int main() {
-	int arr[10] = {5, 9, 2, 1, 6, 3, 8, 4, 7, 0};
-
+	int arr1[10] = {5, 9, 2, 1, 6, 3, 8, 4, 7, 0};
+	std::cout << "arr1: ";       
 	for (int i = 0; i < 10; i++) {
-		std::cout << arr[i] << std::endl;
+		std::cout << arr1[i] << " ";
 	}
+	std::cout << std::endl;
+
+	int arr2[] = {12, 34, 23, 345, 54, 98, 253};
+        std::cout << "arr2: ";
+	for (int i = 0; i < 7; i++) {
+		std::cout << arr2[i] << " ";
+	}
+	std::cout << std::endl;
 
 	return 0;
 }
